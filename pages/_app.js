1b1d66fddea20ed2ff89c1c3bb710d8cf86e6884
boomshakar <hefehmzy@gmail.com>
2022-04-04 23:44:47 +0100

    change spinner color

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import NProgress from "nprogress";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import Layout from "../components/Layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
 	NProgress.configure({ showSpinner: false, trickle: true });
