ab656df29fe5e7fc4d29d3d16727b8068c39f379
Oluwafemi Ṣosanya <Hefehmzy@gmail.com>
2022-04-19 01:11:49 +0100

    Delete .history directory

--- .history/pages/index_20220403051624.js
+++
@@ -1,7 +0,0 @@
-import Link from "next/link";
-import Image from "next/image";
-import { Flex, Box, Text, Button } from "@chakra-ui/react";
-
-export default function Home() {
-    return <div className={styles.container}>Hello</div>;
-}
